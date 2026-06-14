98f718f21095954f85628ffaa3e144deb7b16e30
Byologi <godfredaidoo86@gmail.com>
2026-06-14 16:12:28 +0000

    chore: initialize Idempotency-Key Spring Boot setup

---
+++ src/main/java/com/fredoo/idempotencykey/IdempotencyKeyApplication.java
@@ -0,0 +1,13 @@
+package com.fredoo.idempotencykey;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdempotencyKeyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IdempotencyKeyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fredoo/idempotencykey/IdempotencyKeyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fredoo.idempotencykey;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdempotencyKeyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
